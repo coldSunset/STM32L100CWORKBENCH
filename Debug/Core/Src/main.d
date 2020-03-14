a2450334a7d02e5b01f2f89186e4acc2f0ce51b4
Ravindu Sirimanna <ravsiriman@gmail.com>
2020-03-14 14:42:26 +1100

    repo update

--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -1,8 +1,4 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
- ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal.h \
- ../Core/Inc/stm32l1xx_hal_conf.h \
- ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc.h \
- ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_def.h \
+Core/Src/main.o: ../Core/Src/main.c \
  ../Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h \
  ../Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l100xc.h \
  ../Drivers/CMSIS/Include/core_cm3.h \
@@ -11,6 +7,10 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/CMSIS/Include/cmsis_gcc.h \
  ../Drivers/CMSIS/Include/mpu_armv7.h \
  ../Drivers/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h \
+ ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal.h \
+ ../Core/Inc/stm32l1xx_hal_conf.h \
+ ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc.h \
+ ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_def.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc_ex.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_gpio.h \
@@ -21,18 +21,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash_ex.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash_ramfunc.h \
  ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr.h \
- ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr_ex.h \
- ../Core/Inc/gpio.h ../Core/Inc/main.h
-
-../Core/Inc/main.h:
-
-../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal.h:
-
-../Core/Inc/stm32l1xx_hal_conf.h:
-
-../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc.h:
-
-../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_def.h:
+ ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr_ex.h
 
 ../Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h:
 
@@ -49,6 +38,14 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/CMSIS/Include/mpu_armv7.h:
 
 ../Drivers/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h:
+
+../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal.h:
+
+../Core/Inc/stm32l1xx_hal_conf.h:
+
+../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc.h:
+
+../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_def.h:
 
 ../Drivers/STM32L1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
@@ -71,7 +68,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr.h:
 
 ../Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr_ex.h:
-
-../Core/Inc/gpio.h:
-
-../Core/Inc/main.h:
