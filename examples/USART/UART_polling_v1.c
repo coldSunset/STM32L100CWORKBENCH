758fb3cd836129394f0914ae0ebfb80b48a89fd8
Ravindu Sirimanna <ravsiriman@gmail.com>
2020-09-12 13:24:31 +1000

    uart1 implementation

--- examples/USART/UART_polling_v1.c
+++ examples/USART/UART_polling_v1.c
@@ -22,45 +22,24 @@
 #include "main.h"
 #include "usart.h"
 #include "gpio.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define WELCOME_MSG "Welcome to the STMDISCO management console\r\n"
+
+
+#define WELCOME_MSG "Welcome to the STM32L management console\r\n"
 #define MAIN_MENU   "Select the option you are interested in:\r\n\t1. Toggle LD2 LED\r\n\t2. Read USER BUTTON status\r\n\t3. Clear screen and print this message "
 #define PROMPT "\r\n> "
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
 UART_HandleTypeDef huart1;
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-void SystemClock_Config(void);
 void printWelcomeMessage(void);
-uint8_t processUserInput(uint8_t opt);
 uint8_t readUserInput(void);
+uint8_t processUserInput(uint8_t opt);
 
 /* USER CODE END PFP */
 
@@ -75,46 +54,70 @@ uint8_t readUserInput(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-	uint8_t opt = 0;
-  /* USER CODE END 1 */
+	uint8_t opt =0;
 
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
+
   printMessage:
   	  printWelcomeMessage();
-  /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
+  while(1)
   {
-    /* USER CODE END WHILE */
-		opt = readUserInput();
-		processUserInput(opt);
-		if(opt == 3)
-			goto printMessage;
-    /* USER CODE BEGIN 3 */
+	  opt = readUserInput();
+	  processUserInput(opt);
+	  if(opt == 3)
+		goto printMessage;
   }
-  /* USER CODE END 3 */
+
+
+}
+
+void printWelcomeMessage(void)
+{
+	HAL_UART_Transmit(&huart1, (uint8_t*)"\033[0;0H", strlen("\033[0;0H"), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart1, (uint8_t*)"\033[2J", strlen("\033[2J"), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart1, (uint8_t*)WELCOME_MSG, strlen(WELCOME_MSG), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart1, (uint8_t*)MAIN_MENU, strlen(MAIN_MENU), HAL_MAX_DELAY);
+}
+
+uint8_t readUserInput(void)
+{
+	char readBuf[1];
+	HAL_UART_Transmit(&huart1, (uint8_t*)PROMPT, strlen(PROMPT), HAL_MAX_DELAY);
+	HAL_UART_Receive(&huart1, (uint8_t*)readBuf, 1, HAL_MAX_DELAY);
+	return atoi(readBuf);
+}
+
+uint8_t processUserInput(uint8_t opt) {
+	char msg[30];
+
+	if(!opt || opt > 3)
+		return 0;
+
+	sprintf(msg, "%d", opt);
+	HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+
+	switch(opt) {
+	case 1:
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8);
+		break;
+	case 2:
+		sprintf(msg, "\r\nUSER BUTTON status: %s", HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0) == GPIO_PIN_RESET ? "PRESSED" : "RELEASED");
+		HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+		break;
+	case 3:
+		return 2;
+	};
+
+	return 1;
 }
 
 /**
@@ -158,45 +161,7 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-void printWelcomeMessage(void) {
-	HAL_UART_Transmit(&huart1, (uint8_t*)"\033[0;0H", strlen("\033[0;0H"), HAL_MAX_DELAY);
-	HAL_UART_Transmit(&huart1, (uint8_t*)"\033[2J", strlen("\033[2J"), HAL_MAX_DELAY);
-	HAL_UART_Transmit(&huart1, (uint8_t*)WELCOME_MSG, strlen(WELCOME_MSG), HAL_MAX_DELAY);
-	HAL_UART_Transmit(&huart1, (uint8_t*)MAIN_MENU, strlen(MAIN_MENU), HAL_MAX_DELAY);
-}
 
-uint8_t readUserInput(void) {
-	char readBuf[1];
-
-	HAL_UART_Transmit(&huart1, (uint8_t*)PROMPT, strlen(PROMPT), HAL_MAX_DELAY);
-	HAL_UART_Receive(&huart1, (uint8_t*)readBuf, 1, HAL_MAX_DELAY);
-	return atoi(readBuf);
-}
-
-
-uint8_t processUserInput(uint8_t opt) {
-	char msg[30];
-
-	if(!opt || opt > 3)
-		return 0;
-
-	sprintf(msg, "%d", opt);
-	HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-
-	switch(opt) {
-	case 1:
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-		break;
-	case 2:
-		sprintf(msg, "\r\nUSER BUTTON status: %s", HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET ? "PRESSED" : "RELEASED");
-		HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-		break;
-	case 3:
-		return 2;
-	};
-
-	return 1;
-}
 /* USER CODE END 4 */
 
 /**
